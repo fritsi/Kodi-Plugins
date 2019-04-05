167a35eca252f6ca9f3133e1a7e2fd24a5e50c3e
Daniel Fritsi <ffddani@gmail.com>
2019-04-05 22:02:07 +0200

    Fix, because of inner method variable scoping

--- IFTTTRemote/service.py
+++ IFTTTRemote/service.py
@@ -16,14 +16,14 @@ import xbmcaddon
 from lib.fjson import json_load
 from lib.threads import AtomicValue
 
-__addon = xbmcaddon.Addon()
-__addon_name = __addon.getAddonInfo('name')
+_addon = xbmcaddon.Addon()
+_addon_name = _addon.getAddonInfo('name')
 
 
 # Reads a Kodi addons setting or returns None if the setting is empty
 def get_setting(name):
-    global __addon
-    value = __addon.getSetting(name)
+    global _addon
+    value = _addon.getSetting(name)
     if value == '':
         value = None
     return value
@@ -33,18 +33,18 @@ def get_setting(name):
 # Loading Addon Settings #
 ##########################
 
-__service_port = get_setting('servicePort')
-__user_token = get_setting('userToken')
+_service_port = get_setting('servicePort')
+_user_token = get_setting('userToken')
 
-if __service_port is not None:
-    __service_port = int(__service_port)
+if _service_port is not None:
+    _service_port = int(_service_port)
 
-__spdyn_hostname = get_setting('spdynHost')
-__spdyn_token = get_setting('spdynToken')
-__spdyn_update_interval = get_setting('spdynUpdateIntervalLimit')
+_spdyn_hostname = get_setting('spdynHost')
+_spdyn_token = get_setting('spdynToken')
+_spdyn_update_interval = get_setting('spdynUpdateIntervalLimit')
 
-if __spdyn_update_interval is not None:
-    __spdyn_update_interval = int(__spdyn_update_interval)
+if _spdyn_update_interval is not None:
+    _spdyn_update_interval = int(_spdyn_update_interval)
 
 
 ######################
@@ -54,8 +54,8 @@ if __spdyn_update_interval is not None:
 
 # Displays a notification
 def display_notification(message):
-    global __addon_name
-    xbmc.executebuiltin('Notification({}, {}, {})'.format(__addon_name, message, 3000))
+    global _addon_name
+    xbmc.executebuiltin('Notification({}, {}, {})'.format(_addon_name, message, 3000))
 
 
 # Gets a fields from the JSON supplied in the POST body
@@ -137,19 +137,19 @@ def forward_media(content):
     xbmc.Player().seekTime(position)
 
 
-__exit_triggered = AtomicValue(False)
+_exit_triggered = AtomicValue(False)
 
 
 # Triggers a Kodi exit
 # noinspection PyUnusedLocal
 def exit_kodi(content):
-    global __exit_triggered
-    __exit_triggered.set(True)
+    global _exit_triggered
+    _exit_triggered.set(True)
 
 
 # Some valid constants
-__prev_next = ['previous', 'next']
-__on_off = ['on', 'off']
+_prev_next = ['previous', 'next']
+_on_off = ['on', 'off']
 
 
 def execute_jsonrpc(method, params):
@@ -158,30 +158,30 @@ def execute_jsonrpc(method, params):
 
 # Selects the next or previous subtitle
 def select_subtitle(content):
-    global __prev_next
-    global __on_off
+    global _prev_next
+    global _on_off
     if not is_media_loaded():
         raise Exception('No media loaded')
     params = get_field(content, 'params')
     mode = get_field(params, 'mode')
-    if mode not in __prev_next and mode not in __on_off:
+    if mode not in _prev_next and mode not in _on_off:
         raise Exception('Invalid mode')
     execute_jsonrpc('Player.SetSubtitle', '{{ "playerid": 1, "subtitle": "{}" }}'.format(mode))
 
 
 # Selects the next or previous audio track
 def select_audio(content):
-    global __prev_next
+    global _prev_next
     if not is_media_loaded():
         raise Exception('No media loaded')
     params = get_field(content, 'params')
     mode = get_field(params, 'mode')
-    if mode not in __prev_next:
+    if mode not in _prev_next:
         raise Exception('Invalid mode')
     execute_jsonrpc('Player.SetAudioStream', '{{ "playerid": 1, "stream": "{}" }}'.format(mode))
 
 
-__service_handlers = {
+_service_handlers = {
     'pause': pause_media,
     'resume': resume_media,
     'stop': stop_media,
@@ -208,31 +208,24 @@ def get_local_ip():
 
 # Authorizes the request
 def do_authorization(content):
-    global __user_token
+    global _user_token
     raw_authorization = get_field(content, 'authorization')
     authorization = base64.b64decode(raw_authorization)
     items = authorization.split('/')
     if len(items) != 2:
         raise Exception('Unauthorized')
-    if items[0] != get_local_ip() or items[1] != __user_token:
+    if items[0] != get_local_ip() or items[1] != _user_token:
         raise Exception('Unauthorized')
 
 
 class IFTTTRemoteService(BaseHTTPRequestHandler):
     # noinspection PyPep8Naming
     def do_POST(self):
-        global __service_handlers
-        global __exit_triggered
-
-        # Returns HTTP 500 if an exit was triggered
-        def should_not_handle():
-            if __exit_triggered.get():
-                self.__end_request(500, "Exit was initiated")
-                return True
-            return False
+        global _service_handlers
+        global _exit_triggered
 
         # If an exit was triggered we do not handle the request
-        if should_not_handle():
+        if self.__should_not_handle():
             return
 
         try:
@@ -263,15 +256,15 @@ class IFTTTRemoteService(BaseHTTPRequestHandler):
 
             # Getting the service to execute
             service = result.path[14:]
-            if service not in __service_handlers:
+            if service not in _service_handlers:
                 raise Exception('Invalid service')
 
             # If an exit was triggered we do not handle the request
-            if should_not_handle():
+            if self.__should_not_handle():
                 return
 
             # Executing the service
-            __service_handlers[service](content)
+            _service_handlers[service](content)
 
             # Returning HTTP 200 on successful execution
             self.__end_request(200)
@@ -282,6 +275,14 @@ class IFTTTRemoteService(BaseHTTPRequestHandler):
 
             # Returning HTTP 500 if there was an error
             self.__end_request(500, error)
+
+    # Returns HTTP 500 if an exit was triggered
+    def __should_not_handle(self):
+        global _exit_triggered
+        if _exit_triggered.get():
+            self.__end_request(500, "Exit was initiated")
+            return True
+        return False
 
     def __end_request(self, code, content=None):
         self.send_response(code)
@@ -332,14 +333,14 @@ def get_ip():
 
 # Updates the IP address via SPDYN
 def update_ip():
-    global __addon
-    global __spdyn_hostname
-    global __spdyn_token
+    global _addon
+    global _spdyn_hostname
+    global _spdyn_token
 
     # Checking whether an IP update is necessary or not
     prev_update = get_setting('__prev_ip_update')
 
-    if prev_update is not None and (get_current_time() - from_time_text(prev_update)).total_seconds() < __spdyn_update_interval * 60:
+    if prev_update is not None and (get_current_time() - from_time_text(prev_update)).total_seconds() < _spdyn_update_interval * 60:
         xbmc.log('[IFTTT remote] Not updating the IP address this time', level=xbmc.LOGNOTICE)
         return
 
@@ -349,9 +350,9 @@ def update_ip():
 
     # Updating the IP address
     headers = {
-        'Authorization': 'Basic {}'.format(base64.b64encode('{}:{}'.format(__spdyn_hostname, __spdyn_token)))
+        'Authorization': 'Basic {}'.format(base64.b64encode('{}:{}'.format(_spdyn_hostname, _spdyn_token)))
     }
-    request = urllib2.Request('https://update.spdyn.de/nic/update?hostname={}&myip={}'.format(__spdyn_hostname, my_ip), None, headers)
+    request = urllib2.Request('https://update.spdyn.de/nic/update?hostname={}&myip={}'.format(_spdyn_hostname, my_ip), None, headers)
     response = read_http(request)
 
     # Validating the response
@@ -362,32 +363,58 @@ def update_ip():
     xbmc.log('[IFTTT remote] Successfully updated the IP address', level=xbmc.LOGNOTICE)
 
     # Storing when the last IP update happened
-    __addon.setSetting('__prev_ip_update', to_time_text(get_current_time()))
+    _addon.setSetting('__prev_ip_update', to_time_text(get_current_time()))
 
 
 # Stores whether we've started out IFTTT service or not
-__tcp_server_running = AtomicValue(False)
+_tcp_server_running = AtomicValue(False)
 
 
-# Starts the addon
-def run():
-    global __service_port
-    global __user_token
-    global __spdyn_hostname
-    global __spdyn_token
-    global __spdyn_update_interval
-    global __exit_triggered
-    global __tcp_server_running
+class Main:
+    def run(self):
+        global _service_port
+        global _user_token
+        global _spdyn_hostname
+        global _spdyn_token
+        global _spdyn_update_interval
+        global _exit_triggered
+        global _tcp_server_running
 
-    if __service_port is None or __user_token is None or __spdyn_hostname is None or __spdyn_token is None or __spdyn_update_interval is None:
-        xbmc.log('[IFTTT remote] Missing settings', level=xbmc.LOGWARNING)
-        return
+        if _service_port is None or _user_token is None or _spdyn_hostname is None or _spdyn_token is None or _spdyn_update_interval is None:
+            xbmc.log('[IFTTT remote] Missing settings', level=xbmc.LOGWARNING)
+            return
 
-    # Creating the HTTP Server
-    tcp_server = HTTPServer(('0.0.0.0', __service_port), IFTTTRemoteService)
+        # Creating the HTTP Server
+        self.__tcp_server = HTTPServer(('0.0.0.0', _service_port), IFTTTRemoteService)
+
+        # Executing the server on a different Thread
+        thread = threading.Thread(target=self.__start_service)
+        thread.daemon = True
+        thread.start()
+
+        # Getting the Kodi monitor so we know when to stop the HTTP service
+        monitor = xbmc.Monitor()
+
+        # Starting a loop to monitor when we need to exit
+        while not monitor.abortRequested():
+            if _exit_triggered.get() or monitor.waitForAbort(2):
+                break
+
+        if _tcp_server_running.get():
+            xbmc.log('[IFTTT remote] Stopping the IFTTT remote service', level=xbmc.LOGNOTICE)
+            display_notification('Stopping the IFTTT remote service')
+            self.__tcp_server.shutdown()
+            _tcp_server_running.set(False)
+
+        # If an exit was triggered we shut down Kodi
+        if _exit_triggered.get() and not monitor.abortRequested():
+            xbmc.log('[IFTTT remote] Triggering Kodi shutdown', level=xbmc.LOGNOTICE)
+            xbmc.shutdown()
 
     # Starts the HTTP Server and displays a notification
-    def start_service():
+    def __start_service(self):
+        global _tcp_server_running
+
         try:
             # Updating the IP
             # Failing to update the IP should not block the start
@@ -400,39 +427,15 @@ def run():
             display_notification('Starting the IFTTT remote service')
 
             try:
-                __tcp_server_running.set(True)
-                tcp_server.serve_forever()
+                _tcp_server_running.set(True)
+                self.__tcp_server.serve_forever()
             except:
                 xbmc.log('[IFTTT remote] Error while serving requests\n{}'.format(traceback.format_exc()), level=xbmc.LOGERROR)
                 display_notification('Failed to start the IFTTT remote service')
-                __tcp_server_running.set(False)
+                _tcp_server_running.set(False)
         except:
             xbmc.log('[IFTTT remote] Error in start service\n{}'.format(traceback.format_exc()), level=xbmc.LOGERROR)
 
-    # Executing the server on a different Thread
-    thread = threading.Thread(target=start_service)
-    thread.daemon = True
-    thread.start()
-
-    # Getting the Kodi monitor so we know when to stop the HTTP service
-    monitor = xbmc.Monitor()
-
-    # Starting a loop to monitor when we need to exit
-    while not monitor.abortRequested():
-        if __exit_triggered.get() or monitor.waitForAbort(2):
-            break
-
-    if __tcp_server_running.get():
-        xbmc.log('[IFTTT remote] Stopping the IFTTT remote service', level=xbmc.LOGNOTICE)
-        display_notification('Stopping the IFTTT remote service')
-        tcp_server.shutdown()
-        __tcp_server_running.set(False)
-
-    # If an exit was triggered we shut down Kodi
-    if __exit_triggered.get() and not monitor.abortRequested():
-        xbmc.log('[IFTTT remote] Triggering Kodi shutdown', level=xbmc.LOGNOTICE)
-        xbmc.shutdown()
-
 
 if __name__ == '__main__':
-    run()
+    Main().run()
